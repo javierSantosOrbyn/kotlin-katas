c736f496804c93e98e80389245ee4f15c859baca
jsantos <javier.santos@orbyn.com>
2023-11-23 15:32:14 +0100

    Fix word wrap

--- src/test/kotlin/dev/kata/wordWrap/WordWordWrapShould.kt
+++ src/test/kotlin/dev/kata/wordWrap/WordWordWrapShould.kt
@@ -1,6 +1,7 @@
 package dev.kata.wordWrap
 
 import arrow.core.Either
+import arrow.core.getOrElse
 import arrow.core.left
 import arrow.core.right
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -10,14 +11,14 @@ class Columns private constructor(val columns: Int) {
 
     companion object {
         fun create(columns: String): Either<Exception, Columns> {
-            try {
+            return try {
                 if (columns.toInt() <= 0) {
-                    return Exception("columns has to be one or bigger").left()
+                    Exception("columns has to be one or bigger").left()
                 } else {
-                    return Columns(columns.toInt()).right()
+                    Columns(columns.toInt()).right()
                 }
             } catch (ex: Exception) {
-                return Exception("The input was not a number").left()
+                Exception("The input was not a number").left()
             }
         }
     }
@@ -26,20 +27,20 @@ class Columns private constructor(val columns: Int) {
 class WordWrap {
     companion object {
 
-        fun wrap(value: String, safeColumns: Columns): String {
+        fun wordWrap(fullText: String, safeColumns: Columns): String {
             val column = safeColumns.columns
-            if (value.length > column) {
-                val remainingText = value.substring(0, column)
-                val indexOfLastSpace = remainingText.indexOfLast { it == ' ' }
-
-                if (indexOfLastSpace == -1) {
-                    return remainingText + "\n" + wrap(value.substring(remainingText.length), safeColumns)
-                }
-
-                return remainingText.substring(0, indexOfLastSpace) + "\n" + wrap(value.substring(remainingText.length), safeColumns)
-            } else {
-                return value
+            if (fullText.length <= column) {
+                return fullText.trim()
             }
+
+            var tentativeStr = fullText.substring(0, column)
+
+            if (tentativeStr.contains(" ")) {
+                val lastPositionSpace = tentativeStr.indexOfLast { it == ' ' }
+                tentativeStr = tentativeStr.substring(0, lastPositionSpace)
+            }
+
+            return tentativeStr.trim() + "\n" + wordWrap(fullText.substring(tentativeStr.length).trim(), safeColumns)
         }
 
         @JvmStatic
@@ -52,7 +53,7 @@ class WordWrap {
 
             when (columns) {
                 is Either.Left -> print("Invalid input")
-                is Either.Right -> print("Result -> ${wrap(str, columns.value)}")
+                is Either.Right -> print("Result -> ${wordWrap(str, columns.value)}")
             }
         }
     }
@@ -60,39 +61,53 @@ class WordWrap {
 
 class WordWordWrapShould {
 
-    // casos limite
-    // "hola", 2 -> "ho\nla"
-    // "hola mundo", 8   -> "hola\nmundo"
-    // "abracadabra", 2 -> "ab\nra\ncad\nab\nra"
-    // "hola abracadabra", 3 -> "hol\na\nabr\naca\ndab\nra"
-    // "hola    abracadabra", 3 -> "hol\na\nabr\naca\ndab\nra"
-
     @Test
     fun `Wrap a word with no spaces`() {
-        val response = WordWrap.wrap("hola", Columns.create("2") as Either.Right)
+        val response = runWordWrap("hola", "2")
 
         assertEquals(response, "ho\nla")
     }
 
     @Test
     fun `Wrap a word with spaces and longer`() {
-        val response = WordWrap.wrap("abracadabra", 2)
+        val response = runWordWrap("abracadabra", "2")
 
         assertEquals(response, "ab\nra\nca\nda\nbr\na")
     }
 
     @Test
-    fun `Wrap a word with spaces and max 0`() {
-        val response = WordWrap.wrap("abracadabra", 0)
-
-        assertEquals(response, "abracadabra")
-    }
-
-    @Test
     fun `Wrap a word with spaces and max more than length`() {
-        val response = WordWrap.wrap("abc", 4)
+        val response = runWordWrap("abc", "4")
 
         assertEquals(response, "abc")
     }
 
+    @Test
+    fun `If there is a blank space insert a line break in the blank space`() {
+        val response = runWordWrap("hola mundo", "8")
+
+        assertEquals(response, "hola\nmundo")
+    }
+
+    @Test
+    fun `If there is a blank space insert a line break in the blank space before a long text`() {
+        val response = runWordWrap("hola abracadabra", "3")
+
+        assertEquals(response, "hol\na\nabr\naca\ndab\nra")
+    }
+
+    @Test
+    fun `If there is a long blank space insert a line break in the blank space before a long text`() {
+        val response = runWordWrap("hola    abracadabra", "3")
+
+        assertEquals(response, "hol\na\nabr\naca\ndab\nra")
+    }
+
+    private fun runWordWrap(text: String, validColumn: String): String {
+        return Columns.create(validColumn).map {
+            WordWrap.wordWrap(text, it)
+        }.getOrElse {
+            throw Exception("Please give a valid column more than 0")
+        }
+    }
 }
